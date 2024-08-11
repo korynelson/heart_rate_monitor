dcaa77158b4089ffdfd54be3d59cdab735e6cbf8
Kory Nelson <kory.nelson99@gmail.com>
2024-08-11 11:27:46 -0700

    Add bluetooth pairing button
    Add the button for pairing with bluetooth and logic to activate the browsers web bluetooth api.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const requestBluetoothDevice = async () => {
+    try {
+      await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+      });
+      console.log('bluetooth device selected');
+    } catch (error) {
+      console.error('Error selecting Bluetooth device:', error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Heart Rate Monitor</h1>
+        <button onClick={requestBluetoothDevice}> Connect to Bluetooth Device </button>
       </header>
     </div>
   );
